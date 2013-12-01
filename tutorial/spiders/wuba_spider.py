1fad7c59d065d97c8930b6e43e38b2a08db408fc
twairball <twairball@yahoo.com>
2013-12-02 00:18:38 +0800

    print utf-8 encoding to output wuba.json

--- tutorial/spiders/wuba_spider.py
+++ tutorial/spiders/wuba_spider.py
@@ -3,6 +3,7 @@ from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 from tutorial.items import WubaItem
 
+import codecs
 
 class WubaSpider(BaseSpider):
     name = "wuba"
@@ -15,7 +16,7 @@ class WubaSpider(BaseSpider):
         sel = Selector(response)
         posts = sel.xpath('//div[@id="infolist"]/dl')
         items = []
-
+        file = codecs.open('wuba.json', 'w', 'utf-8')
         for post in posts:
             item = WubaItem()
             item['title'] = post.xpath('dt/a/text()').extract()[0]
@@ -23,6 +24,9 @@ class WubaSpider(BaseSpider):
             item['company'] = post.xpath('dd[@class="w271"]/a/text()').extract()[0]
             item['location'] = post.xpath('dd[@class="w96"]/text()').extract()[0]
             items.append(item)
-
-
+            # print item['title'].encode('utf8')
+            output = "{'title': \"%s\", 'link': \"%s\", 'company': \"%s\", 'location': \"%s\"}\n" % (item['title'], item['link'], item['company'], item['location'])
+            print output
+            file.write(output)
+        file.close()
         return items
