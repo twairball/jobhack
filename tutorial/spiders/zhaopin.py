85ca7d43d9c33faae56c26b76d74f7760feeafbc
twairball <twairball@yahoo.com>
2013-12-02 12:15:59 +0800

    zhaopin.com crawler

---
+++ tutorial/spiders/zhaopin.py
@@ -0,0 +1,31 @@
+# -*- coding=UTF-8 -*-
+from scrapy.spider import BaseSpider
+from scrapy.selector import Selector
+from tutorial.items import PostItem
+
+import codecs
+
+class ZhaopinSpider(BaseSpider):
+    name = "zhaopin"
+    allowed_domains = ["sou.zhaopin.com"]
+    start_urls = [
+        "http://sou.zhaopin.com/Jobs/SearchResult.ashx?jl=%E4%B8%8A%E6%B5%B7&kw=ios"
+    ]
+
+    def parse(self, response):
+        sel = Selector(response)
+        posts = sel.xpath('//tr[contains(@class,"showTR")]')
+        items = []
+        file = codecs.open('zhaopin.json', 'w', 'utf-8')
+        for post in posts:
+            item = PostItem()
+            item['title'] = post.xpath('td[contains(@class,"Jobname")]/a/text()').extract()[0]
+            item['link'] = post.xpath('td[contains(@class,"Jobname")]/a/@href').extract()[0]
+            item['company'] = post.xpath('td[contains(@class,"Companyname")]/a/text()').extract()[0]
+            item['location'] = post.xpath('td[contains(@class,"Companyaddress")]/text()').extract()[0]
+
+            output = "{'title': \"%s\", 'link': \"%s\", 'company': \"%s\", 'location': \"%s\"}\n" % (item['title'], item['link'], item['company'], item['location'])
+            print output
+            file.write(output)
+        file.close()
+        return items
