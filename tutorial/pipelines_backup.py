654dee59a77d775fb73a5fe74239b8a8e28b5266
twairball <twairball@yahoo.com>
2013-12-04 23:49:53 +0800

    mongo db

--- tutorial/pipelines_backup.py
+++ tutorial/pipelines_backup.py
@@ -3,15 +3,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import pymongo
 from datetime import datetime
 from hashlib import md5
 from scrapy import log
 from scrapy.exceptions import DropItem
 from twisted.enterprise import adbapi
 
-MONGODB_SAFE = False
-MONGODB_ITEM_ID_FIELD = "_id"
 
 class TutorialPipeline(object):
     def process_item(self, item, spider):
@@ -95,46 +92,3 @@ class MySQLStorePipeline(object):
         """Generates an unique identifier for a given item."""
         # hash based solely in the url field
         return md5(item['url']).hexdigest()
-
-class MongoDBPipeline(object):
-    def __init__(self, mongodb_server, mongodb_port, mongodb_db, mongodb_collection, mongodb_uniq_key,
-                 mongodb_item_id_field, mongodb_safe):
-        connection = pymongo.Connection(mongodb_server, mongodb_port)
-        self.mongodb_db = mongodb_db
-        self.db = connection[mongodb_db]
-        self.mongodb_collection = mongodb_collection
-        self.collection = self.db[mongodb_collection]
-        self.uniq_key = mongodb_uniq_key
-        self.itemid = mongodb_item_id_field
-        self.safe = mongodb_safe
-
-        if isinstance(self.uniq_key, basestring) and self.uniq_key == "":
-            self.uniq_key = None
-            
-        if self.uniq_key:
-            self.collection.ensure_index(self.uniq_key, unique=True)
-
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        settings = crawler.settings
-        return cls(settings.get('MONGODB_SERVER', 'localhost'), settings.get('MONGODB_PORT', 27017),
-                   settings.get('MONGODB_DB', 'scrapy'), settings.get('MONGODB_COLLECTION', None),
-                   settings.get('MONGODB_UNIQ_KEY', None), settings.get('MONGODB_ITEM_ID_FIELD', MONGODB_ITEM_ID_FIELD),
-                   settings.get('MONGODB_SAFE', MONGODB_SAFE))
-
-
-    def process_item(self, item, spider):
-        if self.uniq_key is None:
-            result = self.collection.insert(dict(item), safe=self.safe)
-        else:
-            result = self.collection.update({ self.uniq_key: item[self.uniq_key] }, { '$set': dict(item) },
-                                            upsert=True, safe=self.safe)
-
-        # If item has _id field and is None
-        if self.itemid in item.fields and not item.get(self.itemid, None):
-            item[self.itemid] = result
-
-        log.msg("Item %s wrote to MongoDB database %s/%s" % (result, self.mongodb_db, self.mongodb_collection),
-                level=log.DEBUG, spider=spider)
-        return item
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -12,17 +12,32 @@ SPIDER_MODULES = ['tutorial.spiders']
 NEWSPIDER_MODULE = 'tutorial.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+#USER_AGENT = 'tutorial (http://www.yourdomain.com)'
 
 
 DEFAULT_ITEM_CLASS = 'tutorial.items.PostItem'
 
 ITEM_PIPELINES = [
-    'tutorial.pipelines.RequiredFieldsPipeline',
-    'tutorial.pipelines.MySQLStorePipeline',
-]
-
+	'tutorial.pipelines.RequiredFieldsPipeline',
+	'tutorial.pipelines.MySQLStorePipeline',
+	'tutorial.pipelines.MongoDBPipeline',
+	# 'tutorial.pipelines.MySQLStorePipeline',
+  ]
+  
 MYSQL_HOST = '127.0.0.1'
 MYSQL_DBNAME = 'jobhack'
 MYSQL_USER = 'jobhack'
 MYSQL_PASSWD = 'j0bh4ck123'
+ 
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DB = 'jobhack'
+MONGODB_COLLECTION = 'job'
+MONGODB_UNIQ_KEY = 'url'
+MONGODB_ITEM_ID_FIELD = '_id'
+MONGODB_SAFE = True
+ 
+ # MYSQL_HOST = '127.0.0.1'
+ # MYSQL_DBNAME = 'jobhack'
+ # MYSQL_USER = 'jobhack'
+ # MYSQL_PASSWD = 'jobhack'
